4415e9f74ca5352df45b92700d2e5b16538fff6f
ShyLee873 <shylee873@gmail.com>
2021-11-22 14:03:51 -0500

    set up basic methods for data management and manipulation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  first_name :string
+#  last_name  :string
+#  email      :string
+#  phone      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  group_id   :integer
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
