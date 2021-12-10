7849040ca595341d88bdd97c9ad1fe269c7df81d
ShyLee873 <shylee873@gmail.com>
2021-12-10 13:59:19 -0500

    added a Post table using scaffold, still a minor issue with routing a link, adde +bootstrap updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts
   resources :groups
   resources :users
+  get '/posts/new/:user_id', to: 'posts#new', as: 'new_user_post'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
