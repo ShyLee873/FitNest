571b4b3f4d543a0d42a0b09057b3f67acdb251c0
Shy L <63942346+ShyLee873@users.noreply.github.com>
2022-01-24 16:12:36 -0500

    Fixed basic stylings of Jumbotron, Navbar collapse, and media query issues

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2022_01_21_201613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["group_id"], name: "index_memberships_on_group_id"
-    t.index ["user_id", "group_id"], name: "index_memberships_on_user_id_and_group_id", unique: true
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
