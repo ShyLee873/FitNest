9e7c131e232ffc18ebd2b1fd12680fcb58392138
kostik700015 <kostik700015@gmail.com>
2022-02-16 17:41:55 -0500

    models tests added

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,7 @@ class Post < ApplicationRecord
   include Authorship
   belongs_to :user
   has_many :likes, dependent: :destroy
+  validates :title, presence: true
+  validates :content, presence: true
   
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 RSpec.describe Group, type: :model do
+    group = Group.new
+  
+  it "is not valid without a name" do
+    group.name = nil
+    expect(group).to_not be_valid
+  end
+
+  it "is not valid without a activity_type" do
+    group.activity_type = nil
+    expect(group).to_not be_valid
+  end
+
+  it "is not valid without a about_us" do
+    group.about_us = nil
+    expect(group).to_not be_valid
+  end
+
+ 
+
+  
+
+
     # subject { Group.new( name: "Rise and Climb", activity_type: "rock climbing", senior: true )}
     # it "is valid with valid attributes" do
     #     expect(subject).to be_valid
@@ -16,4 +38,20 @@ RSpec.describe Group, type: :model do
     #     subject.senior=nil 
     #     expect(subject).to_not be_valid
     # end
+
+
 end
+
+RSpec.describe Group, type: :model do
+       
+    it "is valid with valid attributes" do
+        group = Group.new
+        group.name = "Anything"
+        group.about_us = "Anything"
+        group.activity_type = "Something"
+        group.senior = true
+        group.gym_required = false
+        expect(group).to be_valid
+     end
+
+ end
